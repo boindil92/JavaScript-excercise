353d8206177007736fd8dc9cfcbe221f96e92592
Seba <talahammen@gmail.com>
2023-07-04 01:03:52 +0200

    added test loop.

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -1,4 +1,21 @@
+function testCC() { //Computer Choice
+    
+    for (let i=0; i<20;i++){
+        let result=getComputerChoice();
+        console.log(result);
+    }
 
+}
+
+function testSR() {  //Single Round
+   
+    let result=singleRound();
+
+    for (let i=0; i<5;i++){     
+        console.log(result);
+    }
+
+}
 
 
 function getComputerChoice () {
@@ -14,10 +31,13 @@ function getComputerChoice () {
     }
 }
 
+
+
+
 function singleRound (string) {
    
-    let player = prompt("Please choose your weapon: ","rock,paper or scissors!");
-    let machine = getComputerChoice();
+    const player = prompt("Please choose your weapon: ","rock,paper or scissors!");
+    const machine = getComputerChoice();
     console.log(player);
     console.log(machine);
 
@@ -25,7 +45,7 @@ function singleRound (string) {
         console.log('No one wins :C try again!');
     }
     
-    // const pc=getComputerChoice
+
 
     // if (player==pc){
     //     alert("No one Won :C Try again!");
