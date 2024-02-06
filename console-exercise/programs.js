f9cc0bc3a5b3a2a102b4f660ce80fd07b554bb13
Seba <talahammen@gmail.com>
2024-02-06 10:12:40 +0100

    Ex 1.

--- console-exercise/programs.js
+++ console-exercise/programs.js
@@ -1,1 +1,4 @@
-console.log("hello mondo")
+let namee = "john"; // namee instead on name cos if you put 'let name' on a global scope, it triggers the VScode Corrector
+let admin = namee ;
+
+console.log(admin);
