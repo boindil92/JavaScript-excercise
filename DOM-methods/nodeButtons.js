5928bbfc7850a7ed652ebd34f9c85eae4064d9ac
Seba <talahammen@gmail.com>
2024-03-12 16:28:49 +0100

    passed the buttons experiments.

---
+++ DOM-methods/nodeButtons.js
@@ -0,0 +1,7 @@
+const button=document.querySelector("#btn");
+button.onclick=()=>alert("hello world!"); //arrow function
+
+const bestButton=document.querySelector("#bestButton");
+bestButton.addEventListener("click",()=> {
+    alert("Hello THere!");
+})
