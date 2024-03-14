94a7d381df3352fa06ffb9237bed6c68a387499d
Seba <talahammen@gmail.com>
2024-03-14 16:08:22 +0100

    ended examples, and also implemented with explicit functions

---
+++ DOM-methods/multipleListeners.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll("button");
+
+
+buttons.forEach(buttonListener)
+
+function buttonListener(button) {
+
+    button.addEventListener("click", alertId)
+
+     function alertId() {
+         alert(button.id)
+}
+}
