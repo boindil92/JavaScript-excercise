837f1afb837ac64324387e9c758612c18cacb92a
Seba <talahammen@gmail.com>
2024-03-14 15:34:03 +0100

    implementing buttons' functions.

--- DOM-methods/nodeButtons.js
+++ DOM-methods/nodeButtons.js
@@ -5,3 +5,15 @@ const bestButton=document.querySelector("#bestButton");
 bestButton.addEventListener("click",()=> {
     alert("Hello THere!");
 })
+
+function alertFunction() {
+    alert("YAY YOU DID IT!");
+}
+
+function cool(e){
+    e.target.style.background = "blue";
+    
+}
+
+const callBack=document.querySelector("#callBack");
+callBack.addEventListener("click",cool);
