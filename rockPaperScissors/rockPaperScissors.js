880dfcbfb9ae2fe4fa8cc86e4b6a175c5900152a
Seba <talahammen@gmail.com>
2024-03-16 17:20:28 +0100

    NOW FINALLY WORKS! Damn me and the f SCOPE!

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -102,8 +102,7 @@ function createButtons () {
 
     const rockButton=document.createElement("button");
     rockButton.textContent="Rock!";
-    rockButton.addEventListener("click", () => {player="rock"
-    scoreboard.textContent="PC score= "+pcScore+" Your score= "+playerScore;;});
+    rockButton.addEventListener("click", () => {player="rock";});
     rockButton.addEventListener("click",oneRound);
 
 
@@ -112,8 +111,7 @@ function createButtons () {
 
     const paperButton=document.createElement("button");
     paperButton.textContent="Paper!";
-    paperButton.addEventListener("click", () => {player="paper"
-    scoreboard.textContent="PC score= "+pcScore+" Your score= "+playerScore;;});
+    paperButton.addEventListener("click", () => {player="paper";});
     paperButton.addEventListener("click",oneRound);
 
     
@@ -122,8 +120,7 @@ function createButtons () {
 
     const scissorsButton=document.createElement("button");
     scissorsButton.textContent="Scissors!";
-    scissorsButton.addEventListener("click", () => {player="scissors"
-    scoreboard.textContent="PC score= "+pcScore+" Your score= "+playerScore;;});
+    scissorsButton.addEventListener("click", () => {player="scissors";});
     scissorsButton.addEventListener("click",oneRound);
 
 
@@ -134,50 +131,53 @@ function createButtons () {
 
     upper.appendChild(newDiv);
 
-}
 
-
-
-function oneRound () {
+    function oneRound () {
    
 
     
-    const machine = getComputerChoice();
-    alert('you choose '+player);
-    alert('computer throws '+machine);
-
-
-    if (player==machine) {
-        alert("No one Won :C Try again!");
-
-    } else if (player=='rock' && machine=='paper') {
-        alert('You LOSE! Paper beats Rock!');
-        pcScore=(pcScore+1);
-
-    } else if (player=='rock' && machine=='scissors') {
-        alert('You WIN! Rock beats Scissors!');
-        playerScore=(playerScore+1);
-        
-    } else if (player=='paper'&&machine=='rock') {
-        alert('You WIN! Paper beats Rock!');
-        playerScore=(playerScore+1);
-        
-    } else if (player=='paper'&&machine=='scissors') {
-        alert('You LOSE! Scissors beats Paper!');
-        pcScore=(pcScore+1);
-        
-    } else if (player=='scissors'&&machine=='rock') {
-        alert('You LOSE! Rock beats Scissors!');
-        pcScore=(pcScore+1);
-        
-    } else if (player=='scissors'&&machine=='paper') {
-        alert('You WIN! Scissors beats Paper!');
-        playerScore=(playerScore+1);
-    } else {
-        alert('invalid Selection, please try again!');
-    }
+        const machine = getComputerChoice();
+        alert('you choose '+player);
+        alert('computer throws '+machine);
     
-
+    
+        if (player==machine) {
+            alert("No one Won :C Try again!");
+    
+        } else if (player=='rock' && machine=='paper') {
+            alert('You LOSE! Paper beats Rock!');
+            pcScore=(pcScore+1);
+    
+        } else if (player=='rock' && machine=='scissors') {
+            alert('You WIN! Rock beats Scissors!');
+            playerScore=(playerScore+1);
+            
+        } else if (player=='paper'&&machine=='rock') {
+            alert('You WIN! Paper beats Rock!');
+            playerScore=(playerScore+1);
+            
+        } else if (player=='paper'&&machine=='scissors') {
+            alert('You LOSE! Scissors beats Paper!');
+            pcScore=(pcScore+1);
+            
+        } else if (player=='scissors'&&machine=='rock') {
+            alert('You LOSE! Rock beats Scissors!');
+            pcScore=(pcScore+1);
+            
+        } else if (player=='scissors'&&machine=='paper') {
+            alert('You WIN! Scissors beats Paper!');
+            playerScore=(playerScore+1);
+        } else {
+            alert('invalid Selection, please try again!');
+        }
+        
+        scoreboard.textContent="PC score= "+pcScore+" Your score= "+playerScore;
+    
+    
+    }
 
 
 }
+
+
+
